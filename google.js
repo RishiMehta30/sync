2ab11e8e4090ef2f8024247dbf11dbab99101d2a
Rishi Mehta <rishi@Rishis-MacBook-Pro.local>
2021-01-05 08:49:40 -0500

    added google integration by using firebase ui

---
+++ google.js
@@ -0,0 +1,75 @@
+var mainApp = {};
+(function(){
+var mainContainer = document.getElementById("main_container");
+
+    var logout =  function(){
+        firebase.auth().signOut().then(function(){
+            console.log('success');
+            window.location.replace("login.html");
+        },function(){})
+    }
+
+var init = function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log("stay");
+          mainContainer.style.display = "";
+        } else {
+          // No user is signed in.
+          mainContainer.style.display = "none";
+          console.log("redirect");
+          window.location.replace("login.html");
+        }
+      });
+}
+    
+init();
+})();
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: 'index.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: 'index.html'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+var mainApp = {};
+(function(){
+var mainContainer = document.getElementById("main_container");
+
+    var logtout =  function(){
+        firebase.auth().signOut().then(function(){
+            console.log('success');
+            window.location.replace("login.html");
+        },function(){})
+    }
+
+var init = function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log("stay");
+          mainContainer.style.display = "";
+        } else {
+          // No user is signed in.
+          mainContainer.style.display = "none";
+          console.log("redirect");
+          window.location.replace("login.html");
+        }
+      });
+}
+
+})();
--- index.js
+++ index.js
@@ -53,7 +53,6 @@ function login(){
   });
 
 }
-
 function logout(){
   firebase.auth().signOut();
 }
