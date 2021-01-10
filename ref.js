72c855d306ee6d406e80cbde06d87d6569521753
ashah01 <shaharnav03@gmail.com>
2021-01-10 13:33:09 -0500

    Cleaned html

---
+++ ref.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCdyhc7FtiRBAPt0DkqbUZVqf6KhVv71oU",
+    authDomain: "sync-d6dba.firebaseapp.com",
+    databaseURL: "https://sync-d6dba-default-rtdb.firebaseio.com",
+    projectId: "sync-d6dba",
+    storageBucket: "sync-d6dba.appspot.com",
+    messagingSenderId: "479890973934",
+    appId: "1:479890973934:web:082690f0d2eddd557f7fff",
+    measurementId: "G-YVCPN68ZZX"
+};
+firebase.initializeApp(firebaseConfig);
